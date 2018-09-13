2114f6f4bb59b3efc2bcefd2c49ecb8c985d2fed
Laixdy <fernandoveliz10@gmail.com>
2018-09-12 22:06:16 -0300

    Se pueden añadir productos con nombre personalizado

--- desktop/scripts/inventario.js
+++ desktop/scripts/inventario.js
@@ -1,12 +1,16 @@
 var add_item = document.getElementById("add_item"); //Boton añadir
 var item = document.getElementById("inv"); //Muestrario con items
-// String con html del item a agregar
-var html_item = "<div class=\"col-md-4\"></div>"
 
-var html_itemx = '<div class="col-md-4"><div class="card" style="width: 18rem;"> <div class="card-body"> <h5 class="card-title">Card title</h5> <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p><a href="#" class="card-link">Card link</a> <a href="#" class="card-link">Another link</a> </div></div></div>';
+// String con html del item a agregar
+var html_item = '';
+
+// funcion que hace que el string tenga un nombre y cantidad personalizada
+function html() {
+    html_item = '<div class="col-md-4"><div class="container mt-5"><div class="card" style="width: 18rem;"> <div class="card-body"> <h5 class="card-title">' + prompt("Nombre del producto") + '</h5> <h6 class="card-subtitle mb-2 text-muted">Cantidad disponible: ' + prompt("Cantidad") + '</h6> <a href="#" class="card-link">Reponer</a> <a href="#" class="card-link">Graficos</a> </div></div></div></div>';
+}
 
 // Evento click
 add_item.addEventListener('click', () => {
-    item.innerHTML += html_itemx;
-    alert("objeto añadido")
+    html();
+    item.innerHTML += html_item; //Añade html al ya existente
 });
